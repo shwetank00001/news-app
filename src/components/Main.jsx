dd19a22791b3bcd68e0f9896cae3a9f0e275ff40
shwetank00001 <mshwetank@gmail.com>
2024-01-31 17:30:55 +0530

    gg

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,31 +5,28 @@ const Main = () => {
  
     const [ news, setNews ] = React.useState()
 
-    const data = axios.get(
-        "https://newsapi.org/v2/everything?" +
-            "q=Apple&" +
-            "from=2024-01-31&" +
-            "sortBy=popularity&" +
-            "apiKey=054cf83c023b4c7a839f18b537536a90"
-    );
-
+var value = axios.get("https://newsapi.org/v2/top-headlines?" +
+    "country=us&" +
+    "apiKey=054cf83c023b4c7a839f18b537536a90");
+    
     function getNews(){
         console.log("Test")
-        setNews(data)
+        console.log(value);
+        setNews(value);
     }
 
-    const ele = data.articles.map(function(item){
-        return(
-            <div key={item.id}>
-                <h3>{item}</h3>
-            </div>
-        )
-    })
+    // const ele = data.articles.map(function(item){
+    //     return(
+    //         <div key={item.id}>
+    //             <h3>{item}</h3>
+    //         </div>
+    //     )
+    // })
 
   return (
     <div>
         <button onClick={getNews}>Show news</button>
-        {ele}
+
     </div>
   )
 }
